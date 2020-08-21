718839817bedb952ac36fe6805af5f4df391c80f
Joril <mypopcs@163.com>
2020-08-21 14:29:10 +0800

    Add axios connect backend

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import axios from 'axios'
+
 import App from './App.vue'
-import './plugins/element.js'
+import './plugins/element'
+import './plugins/avue.js'
+
 import router from './router'
 
 Vue.config.productionTip = false
+
+//axios方法获取数据
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:3000'
+})
 
 new Vue({
   router,
---
+++ admin/src/plugins/avue.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Avue from '@smallwei/avue';
+import '@smallwei/avue/lib/index.css';
+
+Vue.use(Avue, { 
+    locale:'zh',
+    theme:'dark',//dark黑色主题 
+})
